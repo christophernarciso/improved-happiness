a921168655ca8891ef1377b52878206d5e83aecd
Chris <divinityforvalor@gmail.com>
2021-07-02 15:46:04 -0400

    Hamburgler animation + mixins
    more sass and burger situations

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+const menu = document.querySelector('#btnMenu');
+
+// Listener for our mobile friendly menu
+menu.addEventListener('click', function() {
+    menu.classList.toggle('open');
+});
