6750980a7e44124ba5a7682632949be5176352ed
Chris <divinityforvalor@gmail.com>
2021-07-02 20:07:20 -0400

    Mobile Menu Complete
    Animations added
    Mobile View complete for now

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,24 @@
+const body = document.querySelector('body');
 const menu = document.querySelector('#btnMenu');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fades = document.querySelectorAll('.has-fade');
 
 // Listener for our mobile friendly menu
 menu.addEventListener('click', function() {
-    menu.classList.toggle('open');
+    // Toggle no scroll on click
+    body.classList.toggle('no-scroll');
+    // Toggle open css
+    const isOpen = header.classList.toggle('open');
+    if (isOpen) {
+        fades.forEach((el) => {
+            el.classList.remove('fade-out');
+            el.classList.add('fade-in');
+        });
+    } else {
+        fades.forEach((el) => {
+            el.classList.remove('fade-in');
+            el.classList.add('fade-out');
+        });
+    }
 });
